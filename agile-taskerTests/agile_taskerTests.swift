3c87841803ab25a8b548cdf788d92e38540c8b3c
Brian Murphy <brianmurphy@gatech.edu>
2016-09-24 01:21:37 -0400

    Initial Commit

---
+++ agile-taskerTests/agile_taskerTests.swift
@@ -0,0 +1,36 @@
+//
+//  agile_taskerTests.swift
+//  agile-taskerTests
+//
+//  Created by Brian Murphy on 9/24/16.
+//  Copyright © 2016 Brian Murphy. All rights reserved.
+//
+
+import XCTest
+@testable import agile_tasker
+
+class agile_taskerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
