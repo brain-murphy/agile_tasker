74a4ba10dfbd7490070a4976780ad384fd6c387b
Abhishek Sen <asen33@gatech.edu>
2016-09-24 02:31:43 -0400

    created Task class

---
+++ Task.swift
@@ -0,0 +1,18 @@
+//
+//  Task.swift
+//  agile-tasker
+//
+//  Created by Abhishek Sen on 9/24/16.
+//  Copyright © 2016 Brian Murphy. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name : String?
+    var dueDate : NSDate?
+    var workLeft : Int?
+    var details : String?
+    
+}
---
+++ agile-tasker/NewTaskView.swift
@@ -0,0 +1,9 @@
+//
+//  NewTaskView.swift
+//  agile-tasker
+//
+//  Created by Abhishek Sen on 9/24/16.
+//  Copyright © 2016 Brian Murphy. All rights reserved.
+//
+
+import Foundation
